288030a343fe0a7f893478f581a4805099447d95
theurichde <16006309+theurichde@users.noreply.github.com>
2026-05-12 23:01:42 +0200

    fix: use actual credential expiration and check json marshal error

--- internal/assume.go
+++ internal/assume.go
@@ -38,11 +38,12 @@ func AssumeDirectly(oidcClient ssooidciface.SSOOIDCAPI, ssoClient ssoiface.SSOAP
 		creds := CredentialProcessOutput{
 			Version:         1,
 			AccessKeyId:     *roleCredentials.RoleCredentials.AccessKeyId,
-			Expiration:      time.Now().Add(1 * time.Hour).Format(time.RFC3339),
+			Expiration:      time.Unix(*roleCredentials.RoleCredentials.Expiration/1000, 0).Format(time.RFC3339),
 			SecretAccessKey: *roleCredentials.RoleCredentials.SecretAccessKey,
 			SessionToken:    *roleCredentials.RoleCredentials.SessionToken,
 		}
-		bytes, _ := json.Marshal(creds)
+		bytes, err := json.Marshal(creds)
+		check(err)
 		_, err = os.Stdout.Write(bytes)
 		check(err)
 	}
