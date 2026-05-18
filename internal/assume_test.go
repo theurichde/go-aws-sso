4b43c27cbdc7b0f4654a40c0d842d6eb0da4d9e6
theurichde <16006309+theurichde@users.noreply.github.com>
2026-05-18 22:32:09 +0200

    test: add credential expiration test for AssumeDirectly stdout

--- internal/assume_test.go
+++ internal/assume_test.go
@@ -1,9 +1,13 @@
 package internal
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
+	"io"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/service/sso"
 	"github.com/aws/aws-sdk-go/service/sso/ssoiface"
@@ -108,4 +112,87 @@ func TestAssumeDirectly(t *testing.T) {
 		t.Errorf("Got: %v, but wanted: %v", got, want)
 	}
 
+}
+
+func TestAssumeDirectly_UsesActualCredentialExpiration(t *testing.T) {
+	os.Remove(os.TempDir() + "/go-aws-sso.lock")
+	temp, err := os.CreateTemp("", "go-aws-sso-assume-directly_")
+	check(err)
+	CredentialsFilePath = temp.Name()
+	defer func(path string) {
+		os.RemoveAll(path)
+		os.Remove(os.TempDir() + "/go-aws-sso.lock")
+	}(CredentialsFilePath)
+
+	expirationUnixMs := time.Now().Add(2 * time.Hour).UnixMilli()
+	expectedExpiration := time.Unix(expirationUnixMs/1000, 0).Format(time.RFC3339)
+
+	dummy := "dummy_assume_directly"
+	accessToken := "AccessToken"
+
+	ssoClient := mockSSOClient{
+		SSOAPI: nil,
+		GetRoleCredentialsOutput: sso.GetRoleCredentialsOutput{RoleCredentials: &sso.RoleCredentials{
+			AccessKeyId:     &dummy,
+			Expiration:      &expirationUnixMs,
+			SecretAccessKey: &dummy,
+			SessionToken:    &dummy,
+		}},
+	}
+
+	expires := int64(0)
+
+	oidcClient := mockSSOOIDCClient{
+		SSOOIDCAPI: nil,
+		CreateTokenOutput: ssooidc.CreateTokenOutput{
+			AccessToken: &accessToken,
+		},
+		RegisterClientOutput: ssooidc.RegisterClientOutput{
+			AuthorizationEndpoint: &dummy,
+			ClientId:              &dummy,
+			ClientSecret:          &dummy,
+			ClientSecretExpiresAt: &expires,
+			TokenEndpoint:         &dummy,
+		},
+		StartDeviceAuthorizationOutput: ssooidc.StartDeviceAuthorizationOutput{
+			DeviceCode:              &dummy,
+			UserCode:                &dummy,
+			VerificationUri:         &dummy,
+			VerificationUriComplete: &dummy,
+		},
+	}
+
+	flagSet := flag.NewFlagSet("test-set", flag.ContinueOnError)
+	flagSet.String("start-url", "foobar", "")
+	flagSet.String("region", "eu-central-1", "")
+	flagSet.String("account-id", "123456", "")
+	flagSet.String("role-name", "super-admin", "")
+	flagSet.String("profile", "default", "")
+	flagSet.Bool("persist", false, "")
+	ctx := cli.NewContext(nil, flagSet, nil)
+
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	AssumeDirectly(oidcClient, ssoClient, ctx)
+
+	w.Close()
+	os.Stdout = oldStdout
+
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, r)
+	check(err)
+
+	var creds CredentialProcessOutput
+	err = json.Unmarshal(buf.Bytes(), &creds)
+	check(err)
+
+	if creds.Expiration != expectedExpiration {
+		t.Errorf("Expiration = %v, want %v", creds.Expiration, expectedExpiration)
+	}
+
+	if creds.Version != 1 {
+		t.Errorf("Version = %v, want 1", creds.Version)
+	}
 }
